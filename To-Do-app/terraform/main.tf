f73b5e10b52eaaacfa31c7fae9b79b90e9cb7bbb
Erikas349 <eerikas46@gmail.com>
2025-06-03 17:12:48 +0000

    deploy yml ?

--- To-Do-app/terraform/main.tf
+++ To-Do-app/terraform/main.tf
@@ -15,25 +15,18 @@ data "aws_security_group" "todo_app_security" {
 }
 
 resource "aws_instance" "TodoApp" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = [data.aws_security_group.todo_app_security.id]
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [data.aws_security_group.todo_app_security.id]
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y git python3-pip
-              pip3 install gunicorn
-              git clone https://github.com/Erikas349/Exam.git
-              cd Exam
-              pip3 install -r requirements.txt
-              gunicorn -b 0.0.0.0:5000 app:app &
-              EOF
-              
   tags = {
     Name = "TodoApp"
+  }
+
+  lifecycle {
+   prevent_destroy = true
   }
 }
