4f6d759ba45f84240c8a2a08318abe5ba00aefcf
Erikas349 <eerikas46@gmail.com>
2025-06-05 20:53:50 +0000

    databas change in app.py

--- To-Do-app/backend/app.py
+++ To-Do-app/backend/app.py
@@ -13,7 +13,7 @@ metrics = PrometheusMetrics(app)
 
 # Database setup
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, 'todo.db')
+DB_PATH = os.path.join(BASE_DIR, 'data', 'todo.db')
 
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH)
