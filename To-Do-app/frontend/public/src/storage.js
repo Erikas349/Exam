49b6503a2491ddc7cdafb454918c4c751fb41aae
Erikas349 <eerikas46@gmail.com>
2025-06-05 17:55:20 +0000

    ffffff

--- To-Do-app/frontend/public/src/storage.js
+++
@@ -1,8 +0,0 @@
-export const Storage = {
-  save(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-  },
-  load(key) {
-    return JSON.parse(localStorage.getItem(key)) || [];
-  }
-};
